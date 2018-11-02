151e7b004d1af494e3888e60c8a6762286308f1c
Vitor Mattos <vitor@php.rio>
2018-11-02 00:35:40 -0300

    webhoo para envio de notificações de novas inscrições

---
+++ webhook.php
@@ -0,0 +1,24 @@
+<?php
+use Telegram\Bot\Api;
+
+require 'vendor/autoload.php';
+
+if(file_exists('.env')) {
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
+}
+
+preg_match('/Olá (?<nome>.*)\r\n/', $_POST['stripped-text'], $matches);
+
+$redis   = new Predis\Client([
+    'host' => getenv('REDIS_HOST'),
+    'port' => getenv('REDIS_PORT')
+]);
+$redis->auth(getenv('REDIS_AUTH'));
+$redis->incr('counter');
+
+$telegram = new Api();
+$telegram->sendMessage([
+    'chat_id' => getenv('CHAT_ID'),
+    'text' => "Mais um: ".$matches['nome']."\nTotal: ".$redis->get('counter')
+]);
